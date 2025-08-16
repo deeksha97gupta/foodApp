2db121d12c8b5ec27da7d5691cd72d096fd162a2
deeksha97gupta <deeksha97gupta@gmail.com>
2025-08-17 00:39:10 +0530

    Adding initial files

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const header = (<h1>Hello React</h1>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(header);
