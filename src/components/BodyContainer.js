a62d3297cb1cbfb2c556076a2a6e600599a38d4c
deeksha97gupta <deeksha97gupta@gmail.com>
2025-08-19 12:17:02 +0530

    Fetch dynamic API

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestroCard from "./RestroCard";
 import { restroData } from "../utils/mockData";
+import Shimmer from "./Shimmer"
 
 const BodyContainer = () => {
-    const [newRestroList, setNewRestroList] = useState(restroData);
+    const [newRestroList, setNewRestroList] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, [])
+    const fetchData = async() => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.918938&lng=75.7887458&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        const dynamicData = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setNewRestroList(dynamicData);
+    }
+    if (newRestroList.length === 0) {
+       return <Shimmer />
+    }
     return (
         <div>
             <div>
