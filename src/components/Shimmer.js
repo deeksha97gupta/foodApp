93e297c21b7316c75b1081870937aafb2dc5e3b0
deeksha97gupta <deeksha97gupta@gmail.com>
2025-08-19 12:45:24 +0530

    Add Loading Skeleton

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,7 +1,17 @@
 const Shimmer = () => {
     return (
         <div className="shimmer-container">
-          <div className="shimmer-card"></div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
+            <div className="shimmer-card">card</div>
         </div>
     )
 }
